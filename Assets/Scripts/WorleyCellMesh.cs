06a91de77438d628a5dfd671063d8e0dca38b2d6
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-05-25 18:20:16 +0100

    refactoring WorleyCellMesh

--- Assets/Scripts/WorleyCellMesh.cs
+++ Assets/Scripts/WorleyCellMesh.cs
@@ -8,75 +8,57 @@ public struct WorleyCellMesh
     public int2 index;
 
     NativeList<Edge> edges;
+    TreeWorleyNoise.CellData currentCell;
 
     public void Execute()
     {
-        TreeWorleyNoise.CellData cell = worley.GetCellData(index, TreeManager.rootFrequency);
-        TreeManager.CreateCube(cell.position + new float3(0,10,0), new float4(1));
+        currentCell = worley.GetCellData(index, TreeManager.rootFrequency);
 
-        GetEdges(cell.index, TreeManager.rootFrequency);
+        GetEdges(currentCell.index, TreeManager.rootFrequency);
+
+        DrawLines();
     }
 
     struct Edge
 	{
-		public float3 midPoint;
+        public float3 midPoint;
 		public float3 edgeDirection;
 		public TreeWorleyNoise.CellData adjacentCell;
 
-		public float3 left{
-			get{
-				return midPoint + edgeDirection;
-			}
-		}
-		public float3 right{
-			get{
-				return midPoint - edgeDirection;
-			}
-		} 
+        public float3 left;
+        public float3 right;
+
+        public Edge(TreeWorleyNoise.CellData currentCell, TreeWorleyNoise.CellData adjacentCell)
+        {
+			float3 offset = adjacentCell.position - currentCell.position;
+
+            this.midPoint = currentCell.position + (offset * 0.5f);
+            this.edgeDirection = math.cross(offset, new float3(0, 1, 0));
+            this.adjacentCell = adjacentCell;
+            this.left = midPoint + edgeDirection;
+            this.right = midPoint - edgeDirection; 
+        }
 	}
 
-    public void GetEdges(int2 cellIndex, float2 frequency)
+    NativeList<Edge> GetEdges(int2 cellIndex, float2 frequency)
 	{
-		TreeWorleyNoise.CellData currentCell = worley.GetCellData(cellIndex, frequency);
-
 		AdjacentIntOffsetsClockwise adjacentOffsets;
-
-		int initialIndex = 0;
-		float smallestOffset = 999999;
 
 		edges = new NativeList<Edge>(Allocator.Temp);
 		for(int i = 0; i < 8; i++)
 		{
 			TreeWorleyNoise.CellData adjacentCell = worley.GetCellData(adjacentOffsets[i] + currentCell.index, frequency);
-
-			float3 o = adjacentCell.position - currentCell.position;
-
-			float3 midPoint = currentCell.position + (o * 0.5f);
-
-			float3 rightAngle = math.cross(o, new float3(0, 1, 0));
-			
-			edges.Add(new Edge{
-				midPoint = midPoint,
-				edgeDirection = rightAngle,
-				adjacentCell = adjacentCell
-			});
-
-			float offsetMagnitude = math.sqrt(o.x*o.x + o.y*o.y + o.z*o.z);
-			if(offsetMagnitude < smallestOffset)
-			{
-				smallestOffset = offsetMagnitude;
-				initialIndex = i;
-			}
+			edges.Add(new Edge(currentCell, adjacentCell));
 		}
+        return edges;
+	}
 
-		int previousIndex = initialIndex;
-		int startIndex = initialIndex < edges.Length-1 ? initialIndex+1 : 0;
-
-		TreeManager.CreateCube(edges[initialIndex].adjacentCell.position, new float4(1, 0, 0, 1));
-
-		for(int i = startIndex; i < edges.Length+startIndex; i++)
+    void DrawLines()
+    {
+        int previousIndex = 7;
+		for(int i = 0; i < 8; i++)
 		{
-			int currentIndex = i < edges.Length ? i : i-edges.Length;
+			int currentIndex = i;
 			int nextIndex = currentIndex < 7 ? currentIndex+1 : 0;
 
 			Edge nextEdge = edges[nextIndex];
@@ -113,13 +95,10 @@ public struct WorleyCellMesh
 			
 			//Draw(previousEdge.midPoint, leftIntersection, lineColor);
 			//Draw(nextEdge.midPoint, rightIntersection, lineColor);
-
-			previousEdge = edge;
 		}
 
 		edges.Dispose();
-
-	}
+    }
 
 	void Draw(float3 parentPosition, float3 childPosition, Color color)
     {
