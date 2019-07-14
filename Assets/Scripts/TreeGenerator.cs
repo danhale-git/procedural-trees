ca5bb76017685f04511d2fb6d8cb964ec21277af
DESKTOP-P30ROAU\Salty <danhale87@gmail.com>
2019-07-14 16:13:03 +0100

    Trunk drawing moved to Trunk

--- Assets/Scripts/TreeGenerator.cs
+++ Assets/Scripts/TreeGenerator.cs
@@ -16,11 +16,11 @@ public struct TreeGenerator
     NativeList<float3> vertices;
     NativeList<int> triangles;
 
-    Leaves leaves;
-    Trunk trunk;
-
     Unity.Mathematics.Random random;
     VectorUtil vectorUtil;
+
+    Leaves leaves;
+    Trunk trunk;
 
     float baseHeight;
 
@@ -32,17 +32,18 @@ public struct TreeGenerator
 
         vertices = new NativeList<float3>(Allocator.Temp);
         triangles = new NativeList<int>(Allocator.Temp);
-
-        leaves = new Leaves(vertices, triangles);
         
         random = new Unity.Mathematics.Random((uint)(parentCell.data.value * 1000));
+
+        leaves = new Leaves(vertices, triangles);
+        trunk = new Trunk(vertices, triangles, random);
 
         baseHeight = simplex.GetSimplex(parentCell.data.position.x, parentCell.data.position.z) * 15;
 
         DrawChildCells(worley.frequency*2);
         //leaves.Draw(parentCellProfile, 0);
 
-        DrawTrunk();
+        trunk.DrawTrunk(parentCell);
 
         DrawCellLines(parentCell);
 
@@ -129,65 +130,7 @@ public struct TreeGenerator
         alreadyChecked.Dispose();
     }
 
-    void DrawTrunk()
-    {
-        float3 min = new float3(-1, 0, -1);
-        float3 max = new float3(1, 0, 1);
-        NativeArray<int> extruded = TrunkVertices(0.2f);
-        extruded = ExtrudeTrunk(extruded, new float3(0, 1, 0), 0.4f);
-        extruded = ExtrudeTrunk(extruded, random.NextFloat3(min, max) + new float3(0, 3, 0), 0.7f);
-        extruded = ExtrudeTrunk(extruded, random.NextFloat3(min, max) + new float3(0, 3, 0), 0.7f);
-        extruded = ExtrudeTrunk(extruded, random.NextFloat3(min, max) + new float3(0, 3, 0), 0.7f);
-        extruded.Dispose(); 
-    }
-
-    NativeArray<int> TrunkVertices(float size)
-    {
-        NativeList<int> trunkIndices = new NativeList<int>(Allocator.Temp);
-
-        for(int i = 0; i < parentCell.vertices.Length; i++)
-        {
-            vertices.Add(parentCell.vertices[i] * size);
-            trunkIndices.Add(vertices.Length-1);
-        }
-
-        NativeArray<int> indexArray = new NativeArray<int>(trunkIndices.Length, Allocator.Temp);
-        indexArray.CopyFrom(trunkIndices);
-        trunkIndices.Dispose();
-        return indexArray;
-    }
-
-    NativeArray<int> ExtrudeTrunk(NativeArray<int> startIndices, float3 extrusion, float scale)
-    {
-        int edgeCount = startIndices.Length;
-
-        NativeArray<int> endIndices = new NativeArray<int>(startIndices.Length, Allocator.Temp);
-
-        for(int i = 0; i < edgeCount; i++)
-        {
-            float3 startVertex = vertices[startIndices[i]];
-            float3 endVertex = (startVertex * scale) + extrusion;
-
-            vertices.Add(endVertex);
-            endIndices[i] = vertices.Length-1;
-        }
-
-        for(int i = 0; i < edgeCount; i++)
-        {
-            int currentEdge = i;
-            int nextEdge = i == edgeCount-1 ? 0 : i+1;
-
-            triangles.Add(startIndices[currentEdge]);
-            triangles.Add(startIndices[nextEdge]);
-            triangles.Add(endIndices[currentEdge]);
-
-            triangles.Add(startIndices[nextEdge]);
-            triangles.Add(endIndices[nextEdge]);
-            triangles.Add(endIndices[currentEdge]);
-        }
-
-        return endIndices;
-    }
+    
 
     void MakeMesh()
     {
---
+++ Assets/Scripts/Trunk.cs
@@ -0,0 +1,81 @@
+﻿using Unity.Mathematics;
+using Unity.Collections;
+
+public struct Trunk
+{
+    NativeList<float3> vertices;
+    NativeList<int> triangles;
+
+    Random random;
+    WorleyNoise.CellProfile parentCell;
+
+    public Trunk(NativeList<float3> vertices, NativeList<int> triangles, Random random)
+    {
+        this.vertices = vertices;
+        this.triangles = triangles;
+        this.random = random;
+        this.parentCell = new WorleyNoise.CellProfile();
+    }
+
+    public void DrawTrunk(WorleyNoise.CellProfile parentCell)
+    {
+        this.parentCell = parentCell;
+        
+        float3 min = new float3(-1, 0, -1);
+        float3 max = new float3(1, 0, 1);
+        NativeArray<int> extruded = TrunkVertices(0.2f);
+        extruded = ExtrudeTrunk(extruded, new float3(0, 1, 0), 0.4f);
+        extruded = ExtrudeTrunk(extruded, random.NextFloat3(min, max) + new float3(0, 3, 0), 0.7f);
+        extruded = ExtrudeTrunk(extruded, random.NextFloat3(min, max) + new float3(0, 3, 0), 0.7f);
+        extruded = ExtrudeTrunk(extruded, random.NextFloat3(min, max) + new float3(0, 3, 0), 0.7f);
+        extruded.Dispose(); 
+    }
+
+    NativeArray<int> TrunkVertices(float size)
+    {
+        NativeList<int> trunkIndices = new NativeList<int>(Allocator.Temp);
+
+        for(int i = 0; i < parentCell.vertices.Length; i++)
+        {
+            vertices.Add(parentCell.vertices[i] * size);
+            trunkIndices.Add(vertices.Length-1);
+        }
+
+        NativeArray<int> indexArray = new NativeArray<int>(trunkIndices.Length, Allocator.Temp);
+        indexArray.CopyFrom(trunkIndices);
+        trunkIndices.Dispose();
+        return indexArray;
+    }
+
+    NativeArray<int> ExtrudeTrunk(NativeArray<int> startIndices, float3 extrusion, float scale)
+    {
+        int edgeCount = startIndices.Length;
+
+        NativeArray<int> endIndices = new NativeArray<int>(startIndices.Length, Allocator.Temp);
+
+        for(int i = 0; i < edgeCount; i++)
+        {
+            float3 startVertex = vertices[startIndices[i]];
+            float3 endVertex = (startVertex * scale) + extrusion;
+
+            vertices.Add(endVertex);
+            endIndices[i] = vertices.Length-1;
+        }
+
+        for(int i = 0; i < edgeCount; i++)
+        {
+            int currentEdge = i;
+            int nextEdge = i == edgeCount-1 ? 0 : i+1;
+
+            triangles.Add(startIndices[currentEdge]);
+            triangles.Add(startIndices[nextEdge]);
+            triangles.Add(endIndices[currentEdge]);
+
+            triangles.Add(startIndices[nextEdge]);
+            triangles.Add(endIndices[nextEdge]);
+            triangles.Add(endIndices[currentEdge]);
+        }
+
+        return endIndices;
+    }
+}
